57f72e806444bc6412abcfaf08c050d60812dfd5
locwa <94048557+locwa@users.noreply.github.com>
2025-09-16 07:09:20 +0800

    added basic layout for product list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import productList from '../../../products.json'
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-
+  products = productList
 }
