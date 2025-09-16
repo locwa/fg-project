f50f27770ba6e9c7f51c9ca92166b88cad3399bd
locwa <94048557+locwa@users.noreply.github.com>
2025-09-16 08:50:15 +0800

    changed button styling

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -15,10 +15,14 @@ export class ProductDetailsComponent {
   productList = productDetails;
   isProductInStock : boolean | null = null
   constructor(private route: ActivatedRoute){}
+  buttonStyle: string | null = null
+  buttonText: string | null = null
 
   ngOnInit(){
     this.productId = Number(this.route.snapshot.paramMap.get('id'))
     this.product = this.productList.find(p => p.id === this.productId)
-    this.isProductInStock = this.product.quantity === 0 ? true : false;
+    this.isProductInStock = this.product.quantity === 0 ? false : true;
+    this.buttonStyle = this.isProductInStock ? "bg-dark text-light py-2 px-4" : "bg-secondary text-light py-2 px-4"
+    this.buttonText = this.isProductInStock ? "Add To Cart" : "Out Of Stock"
   }
 }
